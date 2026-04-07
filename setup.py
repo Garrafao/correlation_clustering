55ee3a252bd78e2257603f7954a36054a62eccbb
garrafao <schlecdk@wiesenweihe.ims.uni-stuttgart.de>
2026-04-07 19:33:33 +0200

    update and strip setup.py

--- setup.py
+++ setup.py
@@ -3,25 +3,17 @@ from setuptools import setup
 setup(
     name='correlation_clustering',
     version='1.0.0',    
-    description='A example Python package',
-    url='https://github.com/shuds13/pyexample',
+    description='A python implementation of Correlation Clustering',
+    url='https://github.com/Garrafao/correlation_clustering',
     author='Dominik Schlechtweg,
     author_email='dominik.schlechtweg@ims.uni-stuttgart.de',
-    license='BSD 2-clause',
-    packages=['pyexample'],
-    install_requires=['mpi4py&gt;=2.0',
-                      'numpy',                     
-                      ],
+    license='BSD 3-clause',
 
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',  
         'Operating System :: POSIX :: Linux',        
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
     ],
 )
