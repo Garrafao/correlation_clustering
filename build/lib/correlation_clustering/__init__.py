fdbb586815e4bc5b130052275fd2958fb96582dc
garrafao <schlecdk@wiesenweihe.ims.uni-stuttgart.de>
2026-04-09 22:49:54 +0200

    remove build

--- build/lib/correlation_clustering/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-correlation_clustering.
-
-A python implementation of Correlation Clustering.
-"""
-
-__version__ = "2.0.0"
-__author__ = 'Dominik Schlechtweg'
-__credits__ = 'Institute for Natural Language Processing, University of Stuttgart'
